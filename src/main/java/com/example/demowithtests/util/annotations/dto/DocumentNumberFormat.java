eab0245c03447f75e4aaa77d1cdf5eecb629d7e0
Rosenko Taina <rosenko.taya@gmail.com>
2023-12-10 22:02:46 +0200

    Homework 7 validation

--- src/main/java/com/example/demowithtests/util/annotations/dto/DocumentNumberFormat.java
+++ src/main/java/com/example/demowithtests/util/annotations/dto/DocumentNumberFormat.java
@@ -2,23 +2,21 @@ package com.example.demowithtests.util.annotations.dto;
 
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * The annotation verifies that the country is spelled correctly.
- * <p>
- * Condition: The string must be two characters long and be an uppercase.
- * <p>
+ * The annotation verifies that the document number is set correctly.
  * Throws: MethodArgumentNotValidException if the string does not meet the conditions.
  */
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
-@Constraint(validatedBy = CountryRightFormedValidator.class)
-public @interface CountryRightFormed {
-    String message() default "Country must be a 3 characters length and uppercase. E.g. UK, CZ, UA";
+@Constraint(validatedBy = DocumentNumberFormatValidator.class)
+public @interface DocumentNumberFormat {
+    String message() default "Number must be start from 0";
 
     Class<?>[] groups() default {};
 
